52eaacf5ce54a1456e2222ee0a5e19a87edfc26b
Sofia Liendro <sofialiendro.s@gmail.com>
2020-09-21 16:50:01 -0300

    Responsive

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const hamburguesa = document.querySelector(".fa-bars")
 const menuResponsive = document.querySelector(".menu-responsive")
 const links = document.querySelectorAll(".link");
 
+const botonProyectos = document.getElementById("boton-tarjetas-proyectos")
+const tarjetasProyectos = document.querySelectorAll(".tarjeta-proyecto")
+
 
 hamburguesa.onclick = () => {
     menuResponsive.classList.toggle("mostrar-menu")
 }
 
+
+for (let tarjeta of tarjetasProyectos) {
+    
+}
