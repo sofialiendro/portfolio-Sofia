fb4fb21a1072e7588290fd6732d9b0c7a773cd5e
Sofia Liendro <sofialiendro.s@gmail.com>
2020-09-21 10:02:15 -0300

    Menú responsive

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const hamburguesa = document.querySelector(".fa-bars")
 const menuResponsive = document.querySelector(".menu-responsive")
+const links = document.querySelectorAll(".link");
 
 
 hamburguesa.onclick = () => {
-    menuResponsive.classList.toggle("no-mostrar-menu")
+    menuResponsive.classList.toggle("mostrar-menu")
+}
+
+for (let link of links) {
+    link.onclick = () => {
+      hamburguesa.onclick();
+    };
 }
